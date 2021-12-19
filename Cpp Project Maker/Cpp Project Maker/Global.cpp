56aa4b2201050521ad00e3cddb61c1c4c363d4bb
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-12-19 17:32:29 +0000

    Move folder

--- Cpp Project Maker/Cpp Project Maker/Global.cpp
+++
@@ -1,9 +0,0 @@
-//This_Should_Work Global file
-// - Only put variables for build and other scripts 
-// - Do not put varible names that contain names from other headers or scripts
-#include "Global.h"
-#include <fstream>
-#include <string>
-#include <stdlib.h>
-std::string ver ="0.1.0.0";
-std::string build_type = "BETA";
