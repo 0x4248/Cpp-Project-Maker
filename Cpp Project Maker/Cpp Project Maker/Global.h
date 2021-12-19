ad81c0e8465324b749649333f6fe1f12698c19ef
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2021-12-19 17:13:12 +0000

    Add files via upload

---
+++ Cpp Project Maker/Cpp Project Maker/Global.h
@@ -0,0 +1,12 @@
+//Cpp Global file
+// - Only put variables for build and other scripts 
+// - Do not put varible names that contain names from other headers or scripts
+#ifndef GLOBALS
+#define GLOBALS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+extern std::string ver;
+extern std::string build_type;
+#endif	
